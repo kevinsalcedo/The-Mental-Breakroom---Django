61415b518dc7a46f15d9a9e0b915954f1c0b2c1e
Kevin Salcedo <kevsalcedo@gmail.com>
2018-08-16 10:35:52 -0500

    added elastic beanstalk

--- djangorest/djangorest/settings.py
+++ djangorest/djangorest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qjrxt8q$y+d_wcyzx!1=@o6dbjli5^(fo42ss24)*=b7ybjb2$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mbdb-env.bwzm9cseur.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
